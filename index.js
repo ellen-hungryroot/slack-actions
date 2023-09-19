3835db55317a8c01457185796044f9279ee71cd7
Ellen Huffman <ellen.huffman@hungryroot.com>
2023-09-19 14:55:23 -0700

    more logging statements

--- index.js
+++ index.js
@@ -51,6 +51,10 @@ const reply = async () => {
   for (const [key, value] of Object.entries(payload)) {
     core.info(`${key}: ${value}`);
   }
+  core.info(`GITHUB PAYLOAD SENDER`);
+  for (const [key, value] of Object.entries(payload.sender)) {
+    core.info(`${key}: ${value}`);
+  }
   const channelId = core.getInput('channel-id');
   core.info(`CHANNEL ID INPUT: ${channelId}`);
   const botToken = core.getInput('slack-bot-token');
@@ -60,15 +64,20 @@ const reply = async () => {
   core.info(`payload.review: ${payload.review}`);
   core.info(`payload.review.user: ${payload.review.user}`);
   core.info(`payload.review.user.name: ${payload.review.user.name}`);
+  core.info(`payload.review.user.email: ${payload.review.user.email}`);
   // eslint-disable-next-line no-eval
   const messages = eval(core.getInput('message'));
   core.info(`EVAL MESSAGE: ${messages}`);
 
   const client = new WebClient(botToken);
+  core.info("CLIENT CREATED")
   const conversations = await client.conversations.history({ token: botToken, channel: channelId })
+  core.info(`CONVERSATIONS FOUND: ${conversations.messages.length}`)
   const message = conversations.messages.find((m)=> m.text.includes(stringMatcher))
+  core.info(`MESSAGE FOUND: ${message}`)
 
   if (message !== undefined){
+    core.info("posting message")
     await client.chat.postMessage({ token: botToken, channel: channelId , thread_ts: message.ts, text: messages})
   } else {
     core.setFailed('Message could not be found');
